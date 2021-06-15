69e54b320b4e79f4909b2e1046273011e01315c5
EricFormar <53233372+EricM76@users.noreply.github.com>
2021-06-15 18:24:50 -0300

    proyecto de ejemplo terminado

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3030;
+const port = 3031;
+const path = require('path');
 
+app.use(express.static('public'));
 
-app.get('/',(req,res) => res.send('bienvenidos a mi sitio web'))
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+app.get('/contact',(req,res) => res.sendFile(path.join(__dirname,'views','contact.html')));
+app.get('/about',(req,res) => res.sendFile(path.join(__dirname,'views','about.html')));
 
 app.listen(port,() => console.log('Servidor corriendo en http://localhost:' + port))
